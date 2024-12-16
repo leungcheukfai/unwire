e752418546bbb06487294edab9f5b4ac480171b5
Omor Faruk Rony <omorfarukrony2@gmail.com>
2024-12-17 02:26:05 +0600

    Genrate Content Test

--- functions/tool-scheduled.ts
+++ functions/tool-scheduled.ts
@@ -1,12 +1,12 @@
-import { revalidateTag } from "next/cache"
-import { config } from "~/config"
-import EmailToolScheduled from "~/emails/tool-scheduled"
-import { sendEmails } from "~/lib/email"
-import { generateContent } from "~/lib/generate-content"
-import { uploadFavicon, uploadScreenshot } from "~/lib/media"
-import { firecrawlClient } from "~/services/firecrawl"
-import { inngest } from "~/services/inngest"
-import { prisma } from "~/services/prisma"
+import { revalidateTag } from "next/cache";
+import { config } from "~/config";
+import EmailToolScheduled from "~/emails/tool-scheduled";
+import { sendEmails } from "~/lib/email";
+import { generateContent } from "~/lib/generate-content";
+import { uploadFavicon, uploadScreenshot } from "~/lib/media";
+import { firecrawlClient } from "~/services/firecrawl";
+import { inngest } from "~/services/inngest";
+import { prisma } from "~/services/prisma";
 
 export const toolScheduled = inngest.createFunction(
   { id: "tool.scheduled", concurrency: { limit: 2 } },
@@ -14,95 +14,110 @@ export const toolScheduled = inngest.createFunction(
 
   async ({ event, step, logger }) => {
     const tool = await step.run("find-tool", async () => {
-      return prisma.tool.findUniqueOrThrow({ where: { slug: event.data.slug } })
-    })
+      return prisma.tool.findUniqueOrThrow({
+        where: { slug: event.data.slug },
+      });
+    });
 
     // Scrape website
     const scrapedData = await step.run("scrape-website", async () => {
-      const data = await firecrawlClient.scrapeUrl(tool.website, { formats: ["markdown"] })
+      const data = await firecrawlClient.scrapeUrl(tool.website, {
+        formats: ["markdown"],
+      });
 
       if (!data.success) {
-        throw new Error(data.error)
+        throw new Error(data.error);
       }
 
-      logger.info(`Scraped website for ${tool.name}`, { data })
+      logger.info(`Scraped website for ${tool.name}`, { data });
 
-      return data
-    })
+      return data;
+    });
 
     // Run steps in parallel
-    // await Promise.all([
-    //   // step.run("generate-content", async () => {
-    //   //   const { categories, alternatives, ...content } = await generateContent(scrapedData)
+    await Promise.all([
+      step.run("generate-content", async () => {
+        const { categories, alternatives, ...content } = await generateContent(
+          scrapedData
+        );
 
-    //   //   return prisma.tool.update({
-    //   //     where: { id: tool.id },
-    //   //     data: {
-    //   //       ...content,
+        return prisma.tool.update({
+          where: { id: tool.id },
+          data: {
+            ...content,
 
-    //   //       categories: {
-    //   //         connectOrCreate: categories.map(({ id }) => ({
-    //   //           where: { toolId_categoryId: { toolId: tool.id, categoryId: id } },
-    //   //           create: { category: { connect: { id } } },
-    //   //         })),
-    //   //       },
+            categories: {
+              connectOrCreate: categories.map(({ id }) => ({
+                where: {
+                  toolId_categoryId: { toolId: tool.id, categoryId: id },
+                },
+                create: { category: { connect: { id } } },
+              })),
+            },
 
-    //   //       alternatives: {
-    //   //         connectOrCreate: alternatives.map(({ id }) => ({
-    //   //           where: { toolId_alternativeId: { toolId: tool.id, alternativeId: id } },
-    //   //           create: { alternative: { connect: { id } } },
-    //   //         })),
-    //   //       },
-    //   //     },
-    //   //   })
-    //   // }),
+            alternatives: {
+              connectOrCreate: alternatives.map(({ id }) => ({
+                where: {
+                  toolId_alternativeId: { toolId: tool.id, alternativeId: id },
+                },
+                create: { alternative: { connect: { id } } },
+              })),
+            },
+          },
+        });
+      }),
 
+      // step.run("upload-favicon", async () => {
+      //   const { id, slug, website } = tool;
+      //   const faviconUrl = await uploadFavicon(
+      //     website,
+      //     `tools/${slug}/favicon`
+      //   );
 
-    //   // step.run("upload-favicon", async () => {
-    //   //   const { id, slug, website } = tool
-    //   //   const faviconUrl = await uploadFavicon(website, `tools/${slug}/favicon`)
+      //   return prisma.tool.update({
+      //     where: { id },
+      //     data: { faviconUrl },
+      //   });
+      // }),
 
-    //   //   return prisma.tool.update({
-    //   //     where: { id },
-    //   //     data: { faviconUrl },
-    //   //   })
-    //   // }),
+      // step.run("upload-screenshot", async () => {
+      //   const { id, slug, website } = tool;
+      //   const screenshotUrl = await uploadScreenshot(
+      //     website,
+      //     `tools/${slug}/screenshot`
+      //   );
 
-    //   // step.run("upload-screenshot", async () => {
-    //   //   const { id, slug, website } = tool
-    //   //   const screenshotUrl = await uploadScreenshot(website, `tools/${slug}/screenshot`)
-
-    //   //   return prisma.tool.update({
-    //   //     where: { id },
-    //   //     data: { screenshotUrl },
-    //   //   })
-    //   // }),
-    // ])
+      //   return prisma.tool.update({
+      //     where: { id },
+      //     data: { screenshotUrl },
+      //   });
+      // }),
+    ]);
 
     // Disconnect from DB
     await step.run("disconnect-from-db", async () => {
-      return prisma.$disconnect()
-    })
+      return prisma.$disconnect();
+    });
 
     // Revalidate cache
     await step.run("revalidate-cache", async () => {
-      revalidateTag("admin-tools")
-      revalidateTag("schedule")
-      revalidateTag(`tool-${tool.slug}`)
-    })
+      revalidateTag("admin-tools");
+      revalidateTag("schedule");
+      revalidateTag(`tool-${tool.slug}`);
+    });
 
     // Send email
     await step.run("send-email", async () => {
-      if (!tool.submitterEmail) return
+      if (!tool.submitterEmail) return;
 
-      const to = tool.submitterEmail
-      const subject = `Great news! ${tool.name} is scheduled for publication on ${config.site.name} 🎉`
+      const to = tool.submitterEmail;
+      const subject = `Great news! ${tool.name} is scheduled for publication on ${config.site.name} 🎉`;
 
       return sendEmails({
         to,
         subject,
         react: EmailToolScheduled({ to, subject, tool }),
-      })
-    })
-  },
-)
+      });
+    });
+  }
+);
