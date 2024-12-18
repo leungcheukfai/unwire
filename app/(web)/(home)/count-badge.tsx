40c9cc9de560364a7350327af6f7e5828a313ade
Omor Faruk Rony <omorfarukrony2@gmail.com>
2024-12-18 21:07:40 +0600

    Change the Brown color to #01DD85

--- app/(web)/(home)/count-badge.tsx
+++ app/(web)/(home)/count-badge.tsx
@@ -1,12 +1,12 @@
-import { formatNumber } from "@curiousleaf/utils"
-import { ToolStatus } from "@prisma/client"
-import { subDays } from "date-fns"
-import Link from "next/link"
-import plur from "plur"
-import { Badge } from "~/components/web/ui/badge"
-import { Ping } from "~/components/web/ui/ping"
-import { cache } from "~/lib/cache"
-import { prisma } from "~/services/prisma"
+import { formatNumber } from "@curiousleaf/utils";
+import { ToolStatus } from "@prisma/client";
+import { subDays } from "date-fns";
+import Link from "next/link";
+import plur from "plur";
+import { Badge } from "~/components/web/ui/badge";
+import { Ping } from "~/components/web/ui/ping";
+import { cache } from "~/lib/cache";
+import { prisma } from "~/services/prisma";
 
 const getCounts = cache(
   async () => {
@@ -16,16 +16,19 @@ const getCounts = cache(
       }),
 
       prisma.tool.count({
-        where: { status: ToolStatus.Published, publishedAt: { gte: subDays(new Date(), 7) } },
+        where: {
+          status: ToolStatus.Published,
+          publishedAt: { gte: subDays(new Date(), 7) },
+        },
       }),
-    ])
+    ]);
   },
   ["tools-count"],
-  { revalidate: 60 * 60 },
-)
+  { revalidate: 60 * 60 }
+);
 
 const CountBadge = async () => {
-  const [count, newCount] = await getCounts()
+  const [count, newCount] = await getCounts();
 
   return (
     <Badge prefix={<Ping />} className="order-first" asChild>
@@ -35,15 +38,18 @@ const CountBadge = async () => {
           : `${formatNumber(count)}+ open source tools`}
       </Link>
     </Badge>
-  )
-}
+  );
+};
 
 const CountBadgeSkeleton = () => {
   return (
-    <Badge prefix={<Ping />} className="min-w-20 order-first pointer-events-none animate-pulse">
+    <Badge
+      prefix={<Ping />}
+      className="min-w-20 order-first pointer-events-none animate-pulse"
+    >
       &nbsp;
     </Badge>
-  )
-}
+  );
+};
 
-export { CountBadge, CountBadgeSkeleton }
+export { CountBadge, CountBadgeSkeleton };
--- components/web/ui/ping.tsx
+++ components/web/ui/ping.tsx
@@ -1,12 +1,12 @@
-import type { ComponentProps } from "react"
-import { cx } from "~/utils/cva"
+import type { ComponentProps } from "react";
+import { cx } from "~/utils/cva";
 
 export const Ping = ({ className, ...props }: ComponentProps<"div">) => {
   return (
-    <div className={cx("relative size-3 text-primary", className)} {...props}>
+    <div className={cx("relative size-3 text-[#01DD85]", className)} {...props}>
       <div className="absolute inset-0 animate-ping rounded-full bg-current opacity-30 pointer-events-none blur-[1px]" />
       <div className="absolute inset-0 animate-pulse rounded-full bg-current opacity-30 pointer-events-none" />
       <div className="absolute inset-[3px] rounded-full bg-current" />
     </div>
-  )
-}
+  );
+};
