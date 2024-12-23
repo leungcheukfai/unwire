f1fb1828f8805ce5dc5322e2c16ef413db4fca16
Omor Faruk Rony <omorfarukrony2@gmail.com>
2024-12-23 13:38:45 +0600

    post hog  trackings added

--- components/web/posthog-pageview.tsx
+++ components/web/posthog-pageview.tsx
@@ -1,31 +1,32 @@
-import { usePathname, useSearchParams } from "next/navigation"
-import { usePostHog } from "posthog-js/react"
-import { Suspense, useEffect } from "react"
+"use client";
+import { usePathname, useSearchParams } from "next/navigation";
+import { usePostHog } from "posthog-js/react";
+import { Suspense, useEffect } from "react";
 
 export function PosthogPageview() {
   return (
     <Suspense>
       <PosthogPageviewClient />
     </Suspense>
-  )
+  );
 }
 
 const PosthogPageviewClient = () => {
-  const pathname = usePathname()
-  const searchParams = useSearchParams()
-  const posthog = usePostHog()
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const posthog = usePostHog();
 
   useEffect(() => {
     if (pathname && posthog) {
-      let url = window.origin + pathname
+      let url = window.origin + pathname;
 
       if (searchParams.toString()) {
-        url = `${url}?${searchParams.toString()}`
+        url = `${url}?${searchParams.toString()}`;
       }
 
-      posthog.capture("$pageview", { $current_url: url })
+      posthog.capture("$pageview", { $current_url: url });
     }
-  }, [pathname, searchParams, posthog])
+  }, [pathname, searchParams, posthog]);
 
-  return null
-}
+  return null;
+};
